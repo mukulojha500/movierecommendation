b34275c3b88345684c2a45affbdf306a3bb0ea87
mukulojha500 <122665729+mukulojha500@users.noreply.github.com>
2023-06-22 19:28:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,22 @@
-import numpy as np
 import pandas as pd
+import pickle
+import zipfile
 from fastapi import FastAPI
 from pydantic import BaseModel
-import pickle
-import json
-import zipfile
-from fastapi.middleware.cors import CORSMiddleware
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from nltk.stem.porter import PorterStemmer
 
 app = FastAPI()
 
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 # Load the preprocessed data and create the movie predictor object
-zip_file_path = r"./tmdb_5000_credits.zip"
+zip_file_path = "./tmdb_5000_credits.zip"
 file_name = "tmdb_5000_credits.csv"
 
 # Load movies and credits data as generators
 movies = pd.read_csv('tmdb_5000_movies.csv', iterator=True)
 credits = pd.read_csv(zip_file_path, compression='zip', iterator=True)
-    
+
 chunk_size = 1000  # Number of rows to process per chunk
 movies_chunk = movies.get_chunk(chunk_size)
 credits_chunk = credits.get_chunk(chunk_size)
@@ -37,9 +27,6 @@ movies.dropna(inplace=True)
 movies.duplicated().sum()
 
 import ast
-from nltk.stem.porter import PorterStemmer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
 def convert(obj):
     l = []
@@ -68,11 +55,13 @@ def fetch_director(obj):
 # Initialize stemmer and vectorizer
 ps = PorterStemmer()
 cv = CountVectorizer(max_features=5000, stop_words='english')
+
 def stem(text):
     y = []
     for i in text.split():
         y.append(ps.stem(i))
     return " ".join(y)
+
 def preprocess_data(chunk):
     chunk['genres'] = chunk['genres'].apply(convert)
     chunk['keywords'] = chunk['keywords'].apply(convert)
@@ -84,7 +73,6 @@ def preprocess_data(chunk):
     chunk['cast'] = chunk['cast'].apply(lambda x: [i.replace(" ", "") for i in x])
     chunk['crew'] = chunk['crew'].apply(lambda x: [i.replace(" ", "") for i in x])
     chunk['tags'] = chunk['overview'] + chunk['genres'] + chunk['keywords'] + chunk['cast'] + chunk['crew']
-    chunk[['movie_id', 'title', 'tags']] = chunk[['movie_id', 'title', 'tags']].astype(str)
     chunk['tags'] = chunk['tags'].apply(lambda x: " ".join(x))
     chunk['tags'] = chunk['tags'].apply(lambda x: x.lower())
     chunk['tags'] = chunk['tags'].apply(stem)
