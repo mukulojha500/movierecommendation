4d78555ad24ea659ebd83f3c84986fe5700125c3
mukulojha500 <122665729+mukulojha500@users.noreply.github.com>
2023-06-22 17:03:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import json
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = pickle.load(open('movie_predictor.sav', 'rb'))
+
+class ModelInput(BaseModel):
+    movie: str
+
+@app.post('/movie_recommendation')
+def movie_recommend(input_parameters: ModelInput):
+    movie = input_parameters.movie
+    predictions = model.recommend(movie)
+    return {'predictions': predictions}
